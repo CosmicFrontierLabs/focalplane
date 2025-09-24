2c6b3cb5925c9da5faadb7d5bcaa3c4386e7d698
MattyG <meawoppl@gmail.com>
2025-09-24 13:22:49 -0700

    Remove unused selection module (#252)

--- simulator/src/algo/mod.rs
+++ simulator/src/algo/mod.rs
@@ -1,10 +1,6 @@
 //! Algorithm module specific to simulator.
 
 pub mod misc;
-pub mod selection;
 
 // Re-export all shared algo functionality
 pub use shared::algo::*;
-
-// Simulator-specific exports
-pub use selection::{GuideStarQuality, GuideStarSelector};
